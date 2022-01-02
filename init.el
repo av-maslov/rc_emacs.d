e44de5a9d43ca013cf3568436830ab0ea7f507e5
Alexandr Maslov <alexandr.maslov@hm.com>
2022-01-02 12:14:46 +0200

    bck

--- init.el
+++ init.el
@@ -108,6 +108,7 @@
    "w" 'save-buffer
    "k" 'kill-buffer
    "l" 'bookmark-bmenu-list
+   "t" 'toggle-window-split
    "." 'dired
    ;;"f" 'counsel-fzf
    "f" 'projectile-find-file
@@ -491,3 +492,33 @@
   :bind (:map projectile-mode-map
               ("s-p" . projectile-command-map)
               ("C-c p" . projectile-command-map)))
+
+
+
+
+(defun toggle-window-split ()
+  ;; https://www.emacswiki.org/emacs/ToggleWindowSplit
+  (interactive)
+  (if (= (count-windows) 2)
+      (let* ((this-win-buffer (window-buffer))
+	     (next-win-buffer (window-buffer (next-window)))
+	     (this-win-edges (window-edges (selected-window)))
+	     (next-win-edges (window-edges (next-window)))
+	     (this-win-2nd (not (and (<= (car this-win-edges)
+					 (car next-win-edges))
+				     (<= (cadr this-win-edges)
+					 (cadr next-win-edges)))))
+	     (splitter
+	      (if (= (car this-win-edges)
+		     (car (window-edges (next-window))))
+		  'split-window-horizontally
+		'split-window-vertically)))
+	(delete-other-windows)
+	(let ((first-win (selected-window)))
+	  (funcall splitter)
+	  (if this-win-2nd (other-window 1))
+	  (set-window-buffer (selected-window) this-win-buffer)
+	  (set-window-buffer (next-window) next-win-buffer)
+	  (select-window first-win)
+	  (if this-win-2nd (other-window 1))))))
+
